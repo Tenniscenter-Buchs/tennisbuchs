ca44c82b7854f236f80d366025c67ad0cc3b970f
Cedric Schwyter <cedricschwyter@bluewin.ch>
2020-10-18 17:45:41 +0200

    Add swagger API docs

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import Twirl from 'hamburger-react';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: null,
+            setOpen: null
+        }
+    }
+
     render() {
         return (
-            <Twirl />
+            <Twirl toggled={this.state.isOpen} toggle={this.state.setOpen} />
         );
     }
 }
--- server.js
+++ server.js
@@ -2,10 +2,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "tennis-buchs.ch API Documentation | Swagger",
+            version: "0.0.1",
+            description: "This is a CRUD (CREATE, READ, UPDATE, DELETE) API application made with Express and documented with Swagger",
+            license: {
+                name: "GNU General Public License v3.0",
+                url: "https://www.gnu.org/licenses/gpl-3.0.html",
+            },
+            contact: {
+                name: "Cedric Schwyter",
+                email: "cedricschwyter@bluewin.ch",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:5000/",
+            },
+        ],
+    },
+    apis: ["./routes/**/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+      "/api-docs",
+      swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
 
 app.use((req, res, next) => {
     console.log(`Request Endpoint: ${req.method} ${req.url}`);
