b8f046d9459e437f4ba6c1771249420093f0f400
Cedric Schwyter <cedricschwyter@bluewin.ch>
2021-05-06 17:59:37 +0200

    Add swagger API docs

--- server.js
+++ server.js
@@ -11,33 +11,43 @@ const port = process.env.PORT || 5000;
 
 const options = {
     definition: {
-        openapi: "3.0.0",
+        openapi: '3.0.0',
         info: {
-            title: "tennis-buchs.ch API Documentation | Swagger",
-            version: "0.0.1",
-            description: "This is a CRUD (CREATE, READ, UPDATE, DELETE) API application made with Express and documented with Swagger",
+            title: 'tennis-buchs.ch API Documentation | Swagger',
+            version: '0.0.1',
+            description:
+                'This is a CRUD (CREATE, READ, UPDATE, DELETE) API application made with Express and documented with Swagger',
             license: {
-                name: "GNU General Public License v3.0",
-                url: "https://www.gnu.org/licenses/gpl-3.0.html",
+                name: 'GNU General Public License v3.0',
+                url: 'https://www.gnu.org/licenses/gpl-3.0.html',
             },
             contact: {
-                name: "Cedric Schwyter",
-                email: "cedricschwyter@bluewin.ch",
+                name: 'Cedric Schwyter',
+                email: 'cedricschwyter@bluewin.ch',
             },
         },
         servers: [
             {
-                url: "http://localhost:5000/",
+                url: 'http://localhost:5000/api/v1',
+                description: 'Local Integration Testing for API Version 1',
+            },
+            {
+                url: 'https://tennisbuchs-staging.herokuapp.com/api/v1',
+                description: 'Public Staging Testing for API Version 1',
+            },
+            {
+                url: 'https://tennisbuchs-production.herokuapp.com/api/v1',
+                description: 'Production for API Version 1',
             },
         ],
     },
-    apis: ["./routes/**/*.js"],
+    apis: ['./routes/**/*.js'],
 };
 
 const specs = swaggerJsdoc(options);
 app.use(
-      "/api-docs",
-      swaggerUi.serve,
+    '/api-docs',
+    swaggerUi.serve,
     swaggerUi.setup(specs, { explorer: true })
 );
 
@@ -47,26 +57,31 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 app.use(cors());
 
 const api = require('./routes/routes');
 app.use('/api/v1/', api);
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+if (
+    process.env.NODE_ENV === 'production' ||
+    process.env.NODE_ENV === 'staging'
+) {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-};
+}
 
 app.get('*', (req, res) => {
-  res.status(200).json({
-        msg: 'Catch All'
+    res.status(200).json({
+        msg: 'Catch All',
     });
 });
 
